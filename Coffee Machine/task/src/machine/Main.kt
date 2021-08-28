9c1342bd0cb68259dbeca2a629218e2c88e4a6d0
Anatoliy <anartius@gmail.com>
2021-08-28 23:09:39 +0200

    Complete of stage three

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,12 +1,35 @@
 package machine
 
 fun main() {
+
+    // User input
+    println("Write how many ml of water the coffee machine has: ")
+    val totalWater = readLine()!!.toInt()
+
+    println("Write how many ml of milk the coffee machine has: ")
+    val totalMilk = readLine()!!.toInt()
+
+    println("Write how many grams of coffee beans " +
+            "the coffee machine has: ")
+    val totalBeansWeight = readLine()!!.toInt()
+
     println("Write how many cups of coffee do you will need: ")
-    val totalCups = readLine()!!.toInt()
-    println("""
-        For $totalCups cups of coffee you will need:
-        ${totalCups * 200} ml of water
-        ${totalCups * 50} ml of milk
-        ${totalCups * 15} g of coffee beans
-    """.trimIndent())
+    val requiredCups = readLine()!!.toInt()
+
+    // Calculation of amounts of cups
+    val availableCups = listOf(
+        totalWater / 200,
+        totalMilk / 50,
+        totalBeansWeight / 15
+    ).minOrNull()!!
+
+    // Output result
+    println(
+        if (availableCups == requiredCups) {
+            "Yes, I can make that amount of coffee"
+        } else if (availableCups > requiredCups) {
+            "Yes, I can make than amount of coffee (and even " +
+                    "${availableCups - requiredCups} more than that)"
+        } else "No, I can make only $availableCups cups of coffee"
+    )
 }
