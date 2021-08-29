2a87eb4064d824d88dfc6348afd8abaf25aaa98d
Anatoliy <anartius@gmail.com>
2021-08-29 18:32:30 +0200

    Complete of stage five

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -3,14 +3,39 @@ package machine
 fun main() {
     val supplies = Supplies(400, 540, 120, 9, 550)
 
-    supplies.printAmount()
-    print("Write action (buy, fill, take): ")
-    when(readLine()!!) {
-        "buy" -> buy(supplies)
-        "fill" -> fill(supplies)
-        "take" -> take(supplies)
-        else -> println("There isn't so action")
+    while (true) {
+        print("Write action (buy, fill, take, remaining, exit): ")
+        when (readLine()!!) {
+            "buy" -> buy(supplies)
+            "fill" -> fill(supplies)
+            "take" -> take(supplies)
+            "remaining" -> {
+                supplies.printAmount()
+                continue
+            }
+            "exit" -> return
+            else -> println("There isn't so action")
+        }
     }
+
+}
+
+fun checkOfSupplies(choice: String, supplies: Supplies): Boolean {
+
+    val coffee = when(choice) {
+        "1" -> listOf(250, 0, 16, 1)
+        "2" -> listOf(350, 75, 20, 1)
+        else -> listOf(200, 100, 12, 1)
+    }
+    val ingredients = listOf("water", "milk", "coffee beans", "cups")
+    val checkOfSupplies = listOf(supplies.water, supplies.milk, supplies.beans, supplies.cups)
+
+    return if ((checkOfSupplies.zip(coffee) { a, b -> a - b }).minOrNull()!! < 0) {
+        for (i in checkOfSupplies.indices) {
+            if (checkOfSupplies[i] - coffee[i] < 0) println("Sorry, not enough ${ingredients[i]}\n")
+        }
+        false
+    } else true
 
 }
 
@@ -18,42 +43,47 @@ fun buy(supplies: Supplies) {
     var choice: String
     while (true) {
         print(
-            "What do you need to buy? 1 - espresso, 2 - latte, 3 - cappuccino: "
+            "\nWhat do you need to buy? 1 - espresso, 2 - latte, 3 - cappuccino, " +
+                    "back - to main menu: "
         )
         choice = readLine()!!
-        if (choice == "1" || choice == "2" || choice == "3") {
+        if (choice == "back") {
+            return
+        }else if (choice == "1" || choice == "2" || choice == "3") {
             break
         } else println("There isn't so option, try again!")
     }
 
-    supplies.cups -= 1
-    when(choice) {
-        "1" -> {
-            supplies.water -= 250
-            supplies.beans -= 16
-            supplies.money += 4
-        }
-        "2" -> {
-            supplies.water -= 350
-            supplies.milk -= 75
-            supplies.beans -= 20
-            supplies.money += 7
-        }
-        else -> {
-            supplies.water -= 200
-            supplies.milk -= 100
-            supplies.beans -= 12
-            supplies.money += 6
+    if (checkOfSupplies(choice, supplies)) {
+        println("I have enough resources, making you a coffee!\n")
+        when (choice) {
+            "1" -> {
+                supplies.water -= 250
+                supplies.beans -= 16
+                supplies.money += 4
+                supplies.cups -= 1
+            }
+            "2" -> {
+                supplies.water -= 350
+                supplies.milk -= 75
+                supplies.beans -= 20
+                supplies.money += 7
+                supplies.cups -= 1
+            }
+            else -> {
+                supplies.water -= 200
+                supplies.milk -= 100
+                supplies.beans -= 12
+                supplies.money += 6
+                supplies.cups -= 1
+            }
         }
     }
-
-    supplies.printAmount()
-
 }
 
 fun fill(supplies: Supplies) {
 
-    print("Write how many ml of water do you want to add: ")
+    print("\nWrite how many ml of water do you want to add: ")
     supplies.water += readLine()!!.toInt()
     print("Write how many ml of milk do you want to add: ")
     supplies.milk += readLine()!!.toInt()
@@ -61,15 +91,14 @@ fun fill(supplies: Supplies) {
     supplies.beans += readLine()!!.toInt()
     print("Write how many disposable cups of coffee do you want to add: ")
     supplies.cups += readLine()!!.toInt()
+    println("")
 
-    supplies.printAmount()
 }
 
 fun take(supplies: Supplies) {
 
-    println("I gave you $${supplies.money}\n")
+    println("\nI gave you $${supplies.money}\n")
     supplies.money = 0
-    supplies.printAmount()
 
 }
 
